60f706615d12810fd3dc62f2392a604537a1d414
Luis Álvarez <luigi>
2025-08-19 18:28:50 -0400

    last commit

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    user = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(blank=True, null=True)
---
+++ django_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Agrega aquí tus URLs específicas de la app
+]
