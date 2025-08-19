76292604c1ef655af27f4ecbc6c9c2ae9fb76753
unknown <migdest@gmail.com>
2025-08-19 18:30:25 -0400

    first commit

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(blanck=True, null=True) 
