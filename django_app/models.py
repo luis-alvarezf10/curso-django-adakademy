186044fa05febf9efe2603bca4944319367c0e34
unknown <migdest@gmail.com>
2025-08-21 18:27:04 -0400

    crear usuario admin

--- django_app/models.py
+++ django_app/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(blanck=True, null=True) 
+    image = models.ImageField(blank=True, null=True) 
+
+    def __str__(self):
+      return self.title
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_project1 project.
+URL configuration for django_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('django_app.urls'))
-    
+    path('', include('django_app.urls')),
 ]
